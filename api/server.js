b9cb8ff62907706d15a423bd2dea854ff667ea1f
Fayzullo <fayzulllokholnazarov4@gmail.com>
2025-10-04 15:20:15 +0500

    server fix bug

--- api/server.js
+++ api/server.js
@@ -2,12 +2,9 @@ import jsonServer from "json-server";
 import path from "path";
 
 const server = jsonServer.create();
-
-// Абсолютный путь к db.json
-const __dirname = path.resolve(); // корень проекта на Vercel
-const router = jsonServer.router(path.join(__dirname, "data/db.json"));
-
+const router = jsonServer.router(path.join(process.cwd(), "data/db.json"));
 const middlewares = jsonServer.defaults();
+
 server.use(middlewares);
 server.use(router);
 
