8dcc0bdf473244289040271b18930a1159d9ac65
Fayzullo <fayzulllokholnazarov4@gmail.com>
2025-10-04 15:15:45 +0500

    server

--- src/api/server.js
+++ src/api/server.js
@@ -1,9 +1,13 @@
 import jsonServer from "json-server";
+import path from "path";
 
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
 
+// Абсолютный путь к db.json
+const __dirname = path.resolve(); // корень проекта на Vercel
+const router = jsonServer.router(path.join(__dirname, "data/db.json"));
+
+const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
